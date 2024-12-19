7b0e54b94c5cc4f773d737c86a51c976a9772ce4
Sebastian Uhl <uhl.sebastian@gmail.com>
2024-12-19 15:19:52 +0100

    add directory change to start_minecraft_server.sh

--- start_minecraft_server.sh
+++ start_minecraft_server.sh
@@ -3,5 +3,8 @@
 export JAVA_HOME=/home/ec2-user/amazon-corretto-23.0.1.8.1-linux-x64/
 export PATH=$PATH:$JAVA_HOME/bin
 
+#chmod +x start_minecraft_server.sh # to make the script executable
+cd minecraft_server/
+
 # Start the server with 3GB of RAM, and write the output to a log file
 java -Xmx3G -Xms3G -jar server.jar nogui 2>&1 | tee -a ~/minecraft.log
